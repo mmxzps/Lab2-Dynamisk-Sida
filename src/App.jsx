9cff3cdbe07308a4cadb7a2da2ac7b5daa195394
mmxzps <mojtaba.mobasheri@hotmail.com>
2024-03-14 14:04:19 +0100

    added slash

--- src/App.jsx
+++ src/App.jsx
@@ -14,10 +14,10 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
-            <Route path="about" element={<About />} />
-            <Route path="projects" element={<Projects />} />
-            <Route path="jobs" element={<Jobs />} />
-            <Route path="educations" element={<Educations />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/jobs" element={<Jobs />} />
+            <Route path="/educations" element={<Educations />} />
           </Route>
         </Routes>
       </BrowserRouter>
